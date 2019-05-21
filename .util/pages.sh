f8a0ab6161d5dd77d6d3481597928cfc66bc94fc
Lukas Fülling <lukas@k40s.net>
2019-05-21 15:09:09 +0200

    👀

--- .util/pages.sh
+++ .util/pages.sh
@@ -16,7 +16,7 @@ if [[ "$TRAVIS_REPO_SLUG" == "lfuelling/soultraps" ]] && [[ "$TRAVIS_JDK_VERSION
   cp -Rf $HOME/javadoc-latest ./javadoc
   git add -f .
   git commit -m "Latest javadoc for travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq pages gh-pages > /dev/null
+  git push -fq origin gh-pages > /dev/null
 
   echo -e "Published Javadoc to gh-pages.\n"
 
